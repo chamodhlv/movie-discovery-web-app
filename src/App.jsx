d1f3c47c4405ee344fbb322cee23ac9109b57974
chamodhlv <chamodh300@gmail.com>
2026-06-14 22:08:50 +0530

    Refactor Search component and update App to use new props; replace hero image and adjust CSS for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import Search from "./components/Search";
+import Search from "./components/Search.jsx";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
   return (
     <main>
       <div className="pattern"></div>
 
       <div className="wrapper">
         <header>
-          <img src="./public/hero.png" alt="Hero Image"></img>
+          <img src="./public/hero5.png" alt="Hero Image"></img>
           <h1>
-            Discover Amazing <span className="text-gradient">Movies</span>
-            Without Endless Searching
+            Discover Amazing <span className="text-gradient">Movies </span>
+            Without the Hassle
           </h1>
         </header>
 
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./public/search.svg" alt="Search Icon"></img>
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
