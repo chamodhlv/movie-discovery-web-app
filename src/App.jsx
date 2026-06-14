273eba3ddc9d033a4a70b5d2d37aa04e3298dbf5
chamodhlv <chamodh300@gmail.com>
2026-06-14 19:13:55 +0530

    Refactor App component structure and add Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Search from "./components/Search";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <img src="./public/hero.png" alt="Hero Image"></img>
+          <h1>
+            Discover Amazing <span className="text-gradient">Movies</span>
+            Without Endless Searching
+          </h1>
+        </header>
+
+        <Search />
+      </div>
+    </main>
   );
 };
 
